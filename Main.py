08c0605bd9b9a9f283ac2a0eb325ebf00b323b36
Ajay Gautam <110757099+gautamajayk@users.noreply.github.com>
2026-07-06 15:17:56 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ import tarfile
 from six.moves import urllib
 
 
-DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml3/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
